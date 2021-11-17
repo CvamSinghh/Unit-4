5333d578b5d3261289a0c1e22f92fe12f618d3ab
Shivam Singh <shivam_fw12_029@masai.school>
2021-11-18 00:29:22 +0530

    assignment express Server

---
+++ Express/expserver.js
@@ -0,0 +1,28 @@
+
+const express = require("express");
+
+const app = express();
+
+
+app.get("/", function(req, res){
+    res.send("Welcome to Masai")
+});
+
+app.get("/about", function(req, res){
+    res.send("Masai is a coding bootcamp school")
+});
+
+app.get("/contact", function(req, res){
+    res.send("If you want to become a software developer in 30 weeks, contact us Masai School.")
+});
+
+app.get("/data", function(req, res){
+    res.sendFile(__dirname+"/MOCK_DATA.json")
+});
+
+
+
+
+app.listen(5000, function(req, res){
+    console.log("server is running at port no.5000");
+});
